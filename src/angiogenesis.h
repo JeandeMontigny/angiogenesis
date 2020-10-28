8cdfc2be308ebe3d36030299e1f8bf27fd3b162a
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2020-10-28 17:47:11 +0100

    wip set up simu

--- src/angiogenesis.h
+++ src/angiogenesis.h
@@ -15,21 +15,48 @@
 #define ANGIOGENESIS_H_
 
 #include "biodynamo.h"
+#include "biology_modules.h"
+#include "core/substance_initializers.h"
+#include "neuroscience/neuroscience.h"
 
 namespace bdm {
 
 inline int Simulate(int argc, const char** argv) {
-  Simulation simulation(argc, argv);
-
-  // Define initial model - in this example: single cell at origin
+  auto set_param = [](Param* param) {
+    // Create an artificial bound for the simulation space
+    param->bound_space_ = true;
+    param->min_bound_ = -100;
+    param->max_bound_ = 250;
+    param->run_mechanical_interactions_ = true;
+  };
+  neuroscience::InitModule();
+  Simulation simulation(argc, argv, set_param);
   auto* rm = simulation.GetResourceManager();
-  auto* cell = new Cell(30);
-  rm->push_back(cell);
 
-  // Run simulation for one timestep
+  ModelInitializer::DefineSubstance(kVEGF, "VEGF", 0.5, 0, 50);
+
+  auto* cell = new neuroscience::NeuronSoma({0,0,0});
+  cell->SetDiameter(2);
+  rm->push_back(cell);
+  auto vessel = cell->ExtendNewNeurite({1,0,0})->GetSoPtr<NeuriteElement>();
+  vessel->SetDiameter(2);
+  vessel->AddBiologyModule(new VascularGrowth_BM());
+  // elongate vessel to set up initial condition
+  for (int i = 0; i < 300; i++) {
+    vessel->ElongateTerminalEnd(100, {1, i*0.01, 0});
+    vessel->RunDiscretization();
+  }
+
+  // create cancerous cells
+  auto* c_cell = new Cell({50, 150, 0});
+  c_cell->SetDiameter(10);
+  c_cell->AddBiologyModule(new VegfSecretion_BM());
+  c_cell->AddBiologyModule(new TumourGrowth_BM());
+  rm->push_back(c_cell);
+
+  // Run simulation
   simulation.GetScheduler()->Simulate(1);
 
-  std::cout << "Simulation completed successfully!" << std::endl;
   return 0;
 }
 
---
+++ src/biology_modules.h
@@ -0,0 +1,142 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) The BioDynaMo Project.
+// All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+#ifndef BIOLOGY_MODULES_H_
+#define BIOLOGY_MODULES_H_
+
+#include "core/biology_module/biology_module.h"
+#include "core/diffusion_grid.h"
+#include "core/sim_object/cell.h"
+#include "neuroscience/neurite_element.h"
+
+namespace bdm {
+
+enum Substances { kVEGF };
+
+// -----------------------------------------------------------------------------
+/// grow vascularity depending on chemical substance
+struct VascularGrowth_BM : public BaseBiologyModule {
+  VascularGrowth_BM() : BaseBiologyModule(gAllEventIds) {}
+
+  /// Default event constructor
+  VascularGrowth_BM(const Event& event, BaseBiologyModule* other,
+                      uint64_t new_oid = 0)
+      : BaseBiologyModule(event, other, new_oid) {}
+
+  /// Create a new instance of this object using the default constructor.
+  BaseBiologyModule* GetInstance(const Event& event, BaseBiologyModule* other,
+                                 uint64_t new_oid = 0) const override {
+    return new VascularGrowth_BM(event, other, new_oid);
+  }
+
+  /// Create a copy of this biology module.
+  BaseBiologyModule* GetCopy() const override {
+    return new VascularGrowth_BM(*this);
+  }
+
+  void Run(SimObject* so) override {
+    // if initial condition not set
+
+    auto* sim = Simulation::GetActive();
+    auto* rm = sim->GetResourceManager();
+
+    if (!init_) {
+      dg_vegf_ = rm->GetDiffusionGrid(kVEGF);
+      init_ = true;
+    }
+
+    auto* vessel = bdm_static_cast<NeuriteElement*>(so);
+
+  } // end run
+
+  private:
+    bool init_ = false;
+    DiffusionGrid* dg_vegf_ = nullptr;
+}; // end VascularGrowth_BM
+
+// -----------------------------------------------------------------------------
+/// Secrete substance at SimObject position
+struct VegfSecretion_BM : public BaseBiologyModule {
+  VegfSecretion_BM() : BaseBiologyModule(gAllEventIds) {}
+
+  /// Default event constructor
+  VegfSecretion_BM(const Event& event, BaseBiologyModule* other,
+                      uint64_t new_oid = 0)
+      : BaseBiologyModule(event, other, new_oid) {}
+
+  /// Create a new instance of this object using the default constructor.
+  BaseBiologyModule* GetInstance(const Event& event, BaseBiologyModule* other,
+                                 uint64_t new_oid = 0) const override {
+    return new VegfSecretion_BM(event, other, new_oid);
+  }
+
+  /// Create a copy of this biology module.
+  BaseBiologyModule* GetCopy() const override {
+    return new VegfSecretion_BM(*this);
+  }
+
+  void Run(SimObject* so) override {
+    auto* sim = Simulation::GetActive();
+    auto* rm = sim->GetResourceManager();
+
+    if (!init_) {
+      dg_vegf_ = rm->GetDiffusionGrid(kVEGF);
+      init_ = true;
+    }
+
+    auto* vessel = bdm_static_cast<NeuriteElement*>(so);
+
+    auto& secretion_position = vessel->GetPosition();
+    dg_vegf_->IncreaseConcentrationBy(secretion_position, 1);
+  }
+
+  private:
+    bool init_ = false;
+    DiffusionGrid* dg_vegf_ = nullptr;
+}; // end VegfSecretion_BM
+
+// -----------------------------------------------------------------------------
+struct TumourGrowth_BM : public BaseBiologyModule {
+  TumourGrowth_BM() : BaseBiologyModule(gAllEventIds) {}
+
+  /// Default event constructor
+  TumourGrowth_BM(const Event& event, BaseBiologyModule* other,
+                      uint64_t new_oid = 0)
+      : BaseBiologyModule(event, other, new_oid) {}
+
+  /// Create a new instance of this object using the default constructor.
+  BaseBiologyModule* GetInstance(const Event& event, BaseBiologyModule* other,
+                                 uint64_t new_oid = 0) const override {
+    return new TumourGrowth_BM(event, other, new_oid);
+  }
+
+  /// Create a copy of this biology module.
+  BaseBiologyModule* GetCopy() const override {
+    return new TumourGrowth_BM(*this);
+  }
+
+  void Run(SimObject* so) override {
+    if (Cell* cell = dynamic_cast<Cell*>(so)) {
+      if (cell->GetDiameter() <= 12) {
+        cell->ChangeVolume(200);
+      } else {
+        cell->Divide();
+      }
+    }
+  } // end Run
+}; // end TumourGrowth_BM
+
+
+}  // namespace bdm
+
+#endif  // BIOLOGY_MODULES_H_
